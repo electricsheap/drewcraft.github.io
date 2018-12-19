be79c4206579771eca6feb8cb59daf08bb6e724d
Andrew Wilson <ajcheyneywilson@gmail.com>
2018-12-19 09:21:44 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -78,5 +78,5 @@ function frame(a) {
 const m = math
 Array.prototype.add = function(obj) {this[this.length]=obj;return this}
 
-setTimeOut(()=>location=location, 10000)
+setTimeout(()=>location=location, 10000)
 $(init)
